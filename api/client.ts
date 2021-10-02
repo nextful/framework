5050950bd79817a7a5be7535764990e8caf39ae5
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2021-10-02 22:48:52 +0000

    Install apollo

---
+++ api/client.ts
@@ -0,0 +1,27 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+export const createClient = (preview = false) => {
+  const token = process.env.CONTENTFUL_ACCESS_TOKEN;
+  const spaceId = process.env.CONTENTFUL_SPACE_ID
+
+  const client = new ApolloClient({
+    uri: `https://graphql.contentful.com/content/v1/spaces/${spaceId}?access_token=${token}`,
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+      },
+      watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+      },
+    },
+  });
+
+  return client;
+};
+
+const client = createClient();
+
+export default client;
