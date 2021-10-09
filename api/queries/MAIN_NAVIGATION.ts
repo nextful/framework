e263483619a67ad9eed6c9194d01b4c52f7fdbe9
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2021-10-09 20:52:14 +0000

    Add main navigation query

--- api/queries/MAIN_NAVIGATION.ts
+++ api/queries/MAIN_NAVIGATION.ts
@@ -1,7 +1,22 @@
 import { gql } from '@apollo/client';
 
 const MAIN_NAVIGATION = gql`
-    
+    query mainNavigation {
+        pageCollection(limit: 1000, order: index_ASC, where:{ mainNavigation: true }) {
+            items {
+                sys {
+                    id
+                }
+                title
+                slug
+                parent {
+                    sys {
+                        id
+                    }
+                }
+            }
+        }
+    }
 `;
 
 export default MAIN_NAVIGATION;
