607dba533810604c347366c1f4ee26bc5c87ef59
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2021-10-09 20:16:45 +0000

    Implement global context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { GlobalContext, InitialValues } from 'GlobalContext';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <GlobalContext.Provider value={InitialValues}>
+      <Component {...pageProps} />
+    </GlobalContext.Provider>
+  );
 };
 
-export default MyApp;
+export default App;
