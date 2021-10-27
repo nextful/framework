ce9501f0cdb0fec77baa6378ce5dffc380af3996
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2021-10-27 16:48:47 +0000

    Integrate fetch index page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
+const fetchIndex = require('./fetchIndexPage');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async redirects() {
+    const redirects = [];
+    const page = await fetchIndex();
+
+    if (page) {
+      redirects.push({
+        source: '/',
+        destination: `/${page}`,
+        permanent: true,
+      })
+    }
+
+    return redirects;
+  },
   reactStrictMode: true,
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
 }
